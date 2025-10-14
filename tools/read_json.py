e8524c8f900074af7f0e4e4ca5dce4f6c0fb71eb
Juan Fortez <juanfortez112@gmail.com>
2025-10-13 21:31:52 -0300

    Update read_json.py

--- tools/read_json.py
+++ tools/read_json.py
@@ -5,7 +5,6 @@ from typing import Any
 def read_json(file_name: str) -> Any:
     try:
         with open(file_name, mode="rt", encoding="utf-8") as file:
-            data = json.load(file)
+            return json.load(file)
     except OSError:
         print('"No se encuentra el archivo"')
-    return data
