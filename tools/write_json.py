71bcde30cb2b0c966afb16a2ba7111a1d33271e5
Guido do Rego <guidodorego@gmail.com>
2025-10-18 00:39:14 -0300

    corrijo detalle de las funciones para leer y guardar json, tambien creo funciones para guardar ddatos y leerlos

--- tools/write_json.py
+++ tools/write_json.py
@@ -1,11 +1,11 @@
 from typing import Any
+import json
 
 
-def write_json(file_name: str, data: Any):
+def write_json(file_path: str, data: Any):
     try:
-        with open(file_name, mode="w") as file:
-            file.write(data)
-        return True
-    except OSError:
-        # TODO: agregar log de error
-        return False
+        with open(file_path, mode="w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)#ensure_ascii=False,
+
+    except Exception as e:
+        return e
