da1ba2acdceb1047a42b2c8c77057808cebbff1a
LeoLaniere <63814242+LeoLaniere@users.noreply.github.com>
2025-11-03 19:27:12 -0300

    ultimas correcciones

--- tools/loggin/main_loggin.py
+++ tools/loggin/main_loggin.py
@@ -1,15 +1,9 @@
-# tools/loggin/main_loggin.py
-import os
 from tools.loggin.admin_loggin import handle_user_loggin
 from tools.json.index import read_json
 
 def loggin() -> bool:
     """Ejecuta el login del administrador."""
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(base_dir, "users.json")
-    #Busca la ruta absoluta del archivo "user.json"
-    users_db = read_json(db_path)[0]
-    return handle_user_loggin(users_db)  #True o False
-
+    users_db,_ = read_json("users.json")
+    return handle_user_loggin(users_db) 
 if __name__ == "__main__":
     loggin()
